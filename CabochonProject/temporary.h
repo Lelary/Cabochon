553a982bc6e917c54466e976c3e3a49321c0fafe
Lelary <lelary@outlook.com>
2016-01-21 11:03:03 +0000

    * 2016. 1. 21 marbleRadius, blockWidth, blockHeight, boardWidth, boardHeight를 명시적으로 표기하고 계산법 변경

--- CabochonProject/temporary.h
+++ CabochonProject/temporary.h
@@ -2,6 +2,7 @@
 #define _TEMPORARY_H
 #include <array>
 #include "components.h"
+#include "mathematics.h"
 /*
 	2016. 1. 10
 	당장 필요한 constant 변수, typedef 구문인데
@@ -14,8 +15,9 @@
 
 namespace temporary
 {
+	using mathematics::scalar;
 	//-> constants 관련 헤더파일로
-	static const unsigned int radius = 3;
+	//static const unsigned int radius = 3;
 
 	/*
 	2016. 1. 16
@@ -26,7 +28,23 @@ namespace temporary
 	maxY는 차후 변경 가능
 	*/
 	static const unsigned int maxX = 8;
-	static const unsigned int maxY = 12;
+	static const unsigned int maxY = 10;
+
+	/*
+	2016. 1. 21.
+	constants
+	*/
+	static const unsigned int boardWidth = 100;
+	static const unsigned int boardHeight = 120;
+	static const unsigned int deckHeight = 30;
+	static const scalar blockWidth = static_cast<scalar>(boardWidth) / static_cast<scalar>(maxX);
+	static const scalar blockHeight = blockWidth;
+	static const scalar marbleRadius = blockWidth * 0.9f / 2.0f;
+	static const unsigned int boardMargin = 10;
+
+	//static const unsigned int ceiling = 10;
+	//static const unsigned int floor = ceiling + boardHeight;
+
 
 	//-> 어디로?
 	typedef std::array<std::array<components::marble_ptr, maxX>, maxY> marble_array;
