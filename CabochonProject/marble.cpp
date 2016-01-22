28a211c17c7bcd2f55d0d678ed984f5bc08caf2c
Lelary <lelary@outlook.com>
2016-01-22 11:27:37 +0000

    * 2016. 1. 22. Marble 클래스에 circularHitTest 메소드 작성.

--- CabochonProject/marble.cpp
+++ CabochonProject/marble.cpp
@@ -65,3 +65,29 @@ void Marble::setGridPosition(IntPosition gridPosition)
 
 	_gridPosition = gridPosition;
 }
+
+bool Marble::circularHitTest(const Marble& anotherMarble) const
+{
+	circularHitTest(*this, anotherMarble);
+}
+bool Marble::circularHitTest(const Marble& marble1, const Marble& marble2)
+{
+	/* 
+		2016. 1. 22.
+		Marble 객체는 Width = Height = 2*radius
+		현재는 radius 멤버변수가 없으므로 이를 이용한다.
+	*/
+	scalar radius1 = marble1.getWidth() / 2.0f;
+	scalar radius2 = marble2.getWidth() / 2.0f;
+
+	scalar distanceSquare =
+		powf((marble2.getCentralPosition()._x - marble1.getCentralPosition()._x), 2.0f)
+		-
+		powf((marble2.getCentralPosition()._y - marble1.getCentralPosition()._y), 2.0f);
+	
+	//제곱연산이 더 빠르다.
+	if (powf(radius1 + radius2, 2.0f) < distanceSquare)
+		return false;
+	else
+		return true;
+}
