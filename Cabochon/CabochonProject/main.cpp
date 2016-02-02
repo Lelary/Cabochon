71c28a7570376f5aa969ea6732db5cbd57ecabdf
Lelary <lelary@outlook.com>
2016-02-02 15:42:36 +0000

    * 2016. 2. 3.
    winmain으로 교체.
    Ch5. 까지 진행된 Engine과 Cabochon Project Attach.
    
    TODO
    주석처리된 기존 main 함수 삭제.

--- Cabochon/CabochonProject/main.cpp
+++ Cabochon/CabochonProject/main.cpp
@@ -1,14 +1,21 @@
+// 2016. 2. 2.
+// dx9_game_engine\winmain.cpp 으로 대체.
+
+/*
 #include "frameworks.h"
 #include "console.h"
 using frameworks::System;
+*/
 /*
 	2016. 1. 7
 	Model-View 모델을 사용하기 위해
 	아래와 같은 형태를 사용한다.
 */
+/*
 int main()
 {
 	System system;
 	return system.main();
 
 }
+*/
