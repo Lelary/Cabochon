830bd7b7d92e92ba75af2ee7e113e2825bd1feb4
Lelary <lelary@outlook.com>
2016-02-17 06:21:17 +0000

    * 2016. 2. 17. MarbleBoard cpp 분리.

---
+++ Cabochon/CabochonProject/marble_board.cpp
@@ -0,0 +1,127 @@
+// 2016. 2. 17.
+#include "marble_board.h"
+
+using components::MarbleColor;
+using components::MarbleBoard;
+using components::BoardState;
+
+MarbleBoard::MarbleBoard()
+{
+}
+MarbleBoard::~MarbleBoard()
+{
+}
+
+MarbleColor MarbleBoard::existMarble(int x, int y)
+{
+	if (_marbles[x][y] != nullptr)
+		return _marbles[x][y]->getColor();
+	else
+		return MarbleColor::None;
+}
+
+bool MarbleBoard::addMarble(int x, int y, marble_ptr marble)
+{
+	if (_marbles[x][y] != nullptr)
+		return false;
+
+	_marbles[x][y] = std::move(marble);
+	_colorCount[(int)marble->getColor()]++;
+	return true;
+}
+
+bool MarbleBoard::removeMarble(int x, int y)
+{
+	if (_marbles[x][y] != nullptr)
+		return false;
+
+	//메모리 해제 전에 해야함.
+	_colorCount[(int)_marbles[x][y]->getColor()]--;
+	_marbles[x][y].reset();
+}
+
+void MarbleBoard::removeRowZero()
+{
+	_marbles.pop_front();
+}
+
+int MarbleBoard::getMarbleCount(MarbleColor c)
+{
+	if (c == MarbleColor::None || c == MarbleColor::Num)
+		throw("getMarbleCount() : Invalid MarbleColor Parameter.");
+
+	return _colorCount[(int)c];
+}
+int MarbleBoard::getMarbleCount()
+{
+	int sum = 0;
+	for (int i : _colorCount)
+	{
+		sum += i;
+	}
+	return sum;
+}
+int MarbleBoard::getHeight()
+{
+	return _marbles.size();
+}
+
+int MarbleBoard::getFloor()
+{
+	for (int row = 0; row < _marbles.size(); row++)
+		for (const marble_ptr& marble : _marbles[row])
+			if (marble != nullptr)
+				return row;				// _marbles의 인덱스.
+
+	// 모두 nullptr일 경우. 이 상황은 게임이 클리어된상태임. 
+	return getHeight();
+}
+
+bool MarbleBoard::gameOver()
+{
+	// 혹은 getFloor()==0
+	for (const marble_ptr& marble : _marbles.front())
+		if (marble != nullptr)
+		{
+			_boardState = BoardState::GameOver;
+			return true;
+		}
+	return false;
+}
+
+bool MarbleBoard::gameClear()
+{
+	// 방법 1 :  if(getMarbleCount()==0)
+	// 방법 2 :  if(getFloor()==getHeight())
+
+	if (getMarbleCount() == 0)
+	{
+		_boardState = BoardState::GameClear;
+		return true;
+	}
+	return false;
+}
+
+BoardState MarbleBoard::dragDown()
+{
+	// remove Row Zero;
+	// Row Zero 가 삭제되면 자동으로 한칸씩 내려온다.
+	// ( 다른 함수에서. )
+	removeRowZero();
+
+	if (gameOver())
+		return _boardState = BoardState::GameOver;
+
+	if (gameClear())
+		return _boardState = BoardState::GameClear;
+
+	// 화면 밖에만 Marble 이 존재. 
+	// 화면에 보일 때 까지 더 끌어내림.
+	int toDrag = getFloor() - 10;
+	if (toDrag > 0)
+	{
+		for (int i = 0; i < toDrag; i++)
+			_marbles.pop_front();
+	}
+	return _boardState = BoardState::Play;
+}
--- Cabochon/CabochonProject/marble_board.h
+++ Cabochon/CabochonProject/marble_board.h
@@ -40,61 +40,20 @@ namespace components
 		//marble_ptr getMarble();
 
 		// Marble 이 있을 경우 MarbleColor 반환, 없을 경우엔 MarbleColor::None 반환.
-		MarbleColor existMarble(int x, int y)
-		{
-			if (_marbles[x][y] != nullptr)
-				return _marbles[x][y]->getColor();
-			else
-				return MarbleColor::None;
-		}
+		MarbleColor existMarble(int x, int y);
 
-		bool addMarble(int x, int y, marble_ptr marble)
-		{
-			if (_marbles[x][y] != nullptr)
-				return false;
-			
-			_marbles[x][y] = std::move(marble);
-			_colorCount[(int)marble->getColor()]++;
-			return true;
-		}
+		bool addMarble(int x, int y, marble_ptr marble);
 
-		bool removeMarble(int x, int y)
-		{
-			if (_marbles[x][y] != nullptr)
-				return false;
-
-			//메모리 해제 전에 해야함.
-			_colorCount[(int)_marbles[x][y]->getColor()]--;	
-			_marbles[x][y].reset();
-		}
+		bool removeMarble(int x, int y);
 
 		// marble의 존재 여부와 상관없이 0번 줄을 삭제한다.
 		// RowZero는 Line 아래에 있는 Row.
-		void removeRowZero()
-		{
-			_marbles.pop_front();
-		}
+		void removeRowZero();
 
-		int getMarbleCount(MarbleColor c)
-		{
-			if (c == MarbleColor::None || c == MarbleColor::Num)
-				throw("getMarbleCount() : Invalid MarbleColor Parameter.");
+		int getMarbleCount(MarbleColor c);
+		int getMarbleCount();
+		int getHeight();
 
-			return _colorCount[(int)c];
-		}
-		int getMarbleCount()
-		{
-			int sum = 0;
-			for (int i : _colorCount)
-			{
-				sum += i;
-			}
-			return sum;
-		}
-		int getHeight()
-		{
-			return _marbles.size();
-		}
 		/*
 		// 이 함수로 알 수 있는 것.
 		// 반환값 :  현재 Marble 이 있는 첫번 째 줄.
@@ -103,67 +62,15 @@ namespace components
 		// 10초과일 경우 화면에 보이는 것이 없으므로 내려줘야함.	// 다른 함수에서.
 		// 탐색할 필요가 없게 하는 것이 좋음.
 		*/
-		int getFloor() 
-		{
-			for (int row = 0; row < _marbles.size(); row++)
-				for (const marble_ptr& marble : _marbles[row])
-					if (marble != nullptr)
-						return row;				// _marbles의 인덱스.
-
-			// 모두 nullptr일 경우. 이 상황은 게임이 클리어된상태임. 
-			return getHeight();
-		}
+		int getFloor();
 
 		// 마지막 줄에 Marble이 하나라도 있으면 true.
-		bool gameOver()
-		{
-			// 혹은 getFloor()==0
-			for (const marble_ptr& marble : _marbles.front())
-				if (marble != nullptr)
-				{
-					_boardState = BoardState::GameOver;
-					return true;
-				}
-			return false;
-		}
+		bool gameOver();
 
 		// Marble이 Board에 남아 있지 않으면 true.
-		bool gameClear()
-		{
-			// 방법 1 :  if(getMarbleCount()==0)
-			// 방법 2 :  if(getFloor()==getHeight())
-			
-			if (getMarbleCount() == 0)
-			{
-				_boardState = BoardState::GameClear;
-				return true;
-			}
-			return false;
-		}
+		bool gameClear();
 
-		BoardState dragDown()
-		{
-			// remove Row Zero;
-			// Row Zero 가 삭제되면 자동으로 한칸씩 내려온다.
-			// ( 다른 함수에서. )
-			removeRowZero();
-
-			if (gameOver())
-				return _boardState = BoardState::GameOver;
-
-			if (gameClear())
-				return _boardState = BoardState::GameClear;
-				
-			// 화면 밖에만 Marble 이 존재. 
-			// 화면에 보일 때 까지 더 끌어내림.
-			int toDrag = getFloor() - 10;
-			if (toDrag > 0)
-			{
-				for (int i = 0; i < toDrag; i++)
-					_marbles.pop_front();
-			}
-			return _boardState = BoardState::Play;
-		}
+		BoardState dragDown();
 
 		void loadBoard(File* file);
 		void buildBoard();
